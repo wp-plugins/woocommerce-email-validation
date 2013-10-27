98501a1e3ddb49588c310ea787ef91dd681214aa
hlashbrooke <hlashbrooke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-27 05:23:26 +0000

    WooCommerce Email Validation v1.2.2 - Adding Dutch translation (translation provided by Bryan Touw)
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-email-validation/tags/1.2.2@794320 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ woocommerce-email-validation.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Email Validation
+ * Version: 1.2.2
+ * Plugin URI: http://wordpress.org/plugins/woocommerce-email-validation/
+ * Description: Adds a 'confirm email address' field to the WooCommerce checkout page.
+ * Author: Hugh Lashbrooke
+ * Author URI: http://www.hughlashbrooke.com/
+ * Requires at least: 3.0
+ * Tested up to: 3.7
+ *
+ * @package WordPress
+ * @author Hugh Lashbrooke
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+require_once( 'classes/class-woocommerce-email-validation.php' );
+
+global $wcceav;
+$wcceav = new WooCommerce_Email_Validation( __FILE__ );
